145c1d1c91f0abb0f480e8683418e885898a425a
CommanderRhode <duncanfortescue@googlemail.com>
2015-06-18 23:40:55 +0100

    slimer gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,15 +3,6 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
     'ftp-deploy': {
       build: {
         auth: {
@@ -26,8 +17,6 @@ module.exports = function(grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-ftp-deploy');
 
   // Default task(s).
